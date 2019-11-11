93ea6f35e85e0183380f403b4d4d0a6ed0c66163
Samuel Abreu <samuelapp@outlook.com>
2019-11-11 18:17:11 -0300

    Hotfix, 24 motores ao invés de 20

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int main(){
     inicial.x = 3.7;
     inicial.y = 2.4;
     inicial.theta = 0;
-    RobotSPL r1(20,4,50,"Stand",inicial,ATACANTE,PLAYING); //nao deve rodar pq os defines tao na classe, nao na main;
+    RobotSPL r1(24,4,50,"Stand",inicial,ATACANTE,PLAYING); //nao deve rodar pq os defines tao na classe, nao na main;
     int i=0;
     while(i<10){
         r1.movimentacao();
